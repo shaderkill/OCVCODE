4058669398b1de3527c342e6685369cf2c3d565f
shaderkill <cristianmolinaseguel@gmail.com>
2019-06-04 16:41:33 -0400

    update ocv_ipcamera.py

--- ocv_ipcamera/main/OCV_IPCAMERA.py
+++ ocv_ipcamera/main/OCV_IPCAMERA.py
@@ -7,6 +7,13 @@ import time
 import urllib
 from urllib.request import urlopen
 from PIL import Image
+import os
+from os import path
+if __name__ == '__main__':
+    import sys
+    sys.path.append(path.join(path.dirname(__file__), '..'))
+#-- Llamar imports fuera de la carpeta main
+
 
 def numpy2pil(np_array: np.ndarray) -> Image:
     assert_msg = 'Input'
@@ -17,28 +24,36 @@ def numpy2pil(np_array: np.ndarray) -> Image:
     return img
 
 def detectAndDisplay(frame):
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray = cv2.equalizeHist(frame_gray)
+    frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_gray = cv2.equalizeHist(frame_color)
+
     #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray, 1.05, 8)
+    faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv2.ellipse(frame, center, (w//1, h//1),
-                           0, 0, 360, (255, 0, 255), 2)
+        center = (x, y)
         faceROI = frame_gray[y:y+h, x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI, 1.2, 8)
+        faceColor = frame[y:y+h, x:x+w]
+        frame = cv2.rectangle(frame, center, (x+w,y+h), (0, 255, 0), 2)
+        faces_xywh = str(x),str(y),str(w),str(h)
+        faces_str = str(faces_xywh)
+        cv2.putText(frame,faces_str, (x-5,y-5),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
+        #-- Detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI)
         for (x2, y2, w2, h2) in eyes:
             eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
+            radius = int((w2 + h2)*0.25)
             cv2.circle(frame, eye_center, radius, (255, 0, 0), 2)
-        #-- In each face, detect smile
-        smile = smile_cascade.detectMultiScale(faceROI, 1.5, 16)
-        for (x2, y2, w2, h2) in smile:
-            cv2.rectangle(faceROI, (x2, y2), ((x2+w2), (y2+h2)), (0, 255, 0), 2)
-    cv2.imshow('Capture - Face detection', frame)
+    cv2.imshow('Face detection', frame)
+#Función modificada por Cristian 04-06-2019
 
 def tryload():
+    #-- Info CPU, Optimization OPENCV
+    print("-"*100)
+    print("Threads:",cv2.getNumThreads())
+    print("Cores:",cv2.getNumberOfCPUs())
+    print("Optimized:",cv2.useOptimized())
+    print("-"*100)
+    #Info CPU modificado por Cristian 04-06-2019
     #-- Load the cascades
     if not face_cascade.load(cv2.samples.findFile(face_cascade_name)):
         print('--(!)Error loading face cascade')
@@ -57,18 +72,32 @@ def readvideo():
     img = cv2.imdecode(imgNp,-1)
     return img
 
-# Define arguments parse
+
+def playvideo():
+    while True:
+        img = readvideo()
+        if img is None:
+            print('--(!) No captured frame -- Break!')
+            break
+        cv2.useOptimized()
+        detectAndDisplay(img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+#-- Función modificada por Cristian 04-06-2019
+
+#-- Define arguments parse
 parser = argparse.ArgumentParser(
     description='Code for Cascade Classifier tutorial.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', 
                     default='D:\\opencv\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface_improved.xml')
 parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', 
-                    default='D:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_eye.xml')
+                    default='D:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
 parser.add_argument('--smile_cascade', help='Path to smile cascade.', 
                     default='D:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_smile.xml')
 parser.add_argument(
-    '--urlvideo', help='Video streaming.', default='http://192.168.101.100:8080/shot.jpg')
+    '--urlvideo', help='Video streaming.', default='http://10.113.1.63:8080/shot.jpg')
 
+#-- Proceso principal
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
@@ -78,11 +107,4 @@ face_cascade = cv2.CascadeClassifier()
 eyes_cascade = cv2.CascadeClassifier()
 smile_cascade = cv2.CascadeClassifier()
 tryload()
-while True:
-    img = readvideo()
-    if img is None:
-        print('--(!) No captured frame -- Break!')
-        break
-    detectAndDisplay(img)
-    if cv2.waitKey(1) == ord('q'):
-        break
+playvideo()
