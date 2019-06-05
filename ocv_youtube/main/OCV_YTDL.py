c6f1498186f26d98f7c93d689c906712b07be6d0
shaderkill <cristianmolinaseguel@gmail.com>
2019-06-05 12:56:31 -0400

    update OCV YTDL

--- ocv_youtube/main/OCV_YTDL.py
+++ ocv_youtube/main/OCV_YTDL.py
@@ -26,8 +26,12 @@ def tryload():
     print("-"*100)
     #Info CPU modificado por Cristian 04-06-2019
     #-- Load the cascades
-    file = face_cascades.read()
-    if not face_cascade.load(cv2.samples.findFile(file)):
+    file = frontalface_cascades.read()
+    if not frontalface_cascade.load(cv2.samples.findFile(file)):
+        print('--(!)Error cargando "face cascade"')
+        exit(0)
+    file = profileface_cascades.read()
+    if not profileface_cascade.load(cv2.samples.findFile(file)):
         print('--(!)Error cargando "face cascade"')
         exit(0)
     file = eyes_cascades.read()
@@ -43,30 +47,43 @@ def tryload():
 def detectAndDisplay(frame):
     frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame_gray = cv2.equalizeHist(frame_color)
-    #-- Detectar caras
-    faces = face_cascade.detectMultiScale(frame_gray, 1.25, 5)
-    for (x, y, w, h) in faces:
-        top_left = (x, y)
-        botom_right = (x+w,y+h)
-        faceROI = frame_gray[y:y+h, x:x+w]
-        faceColor = frame[y:y+h, x:x+w]
-        frame = cv2.rectangle(frame, top_left, botom_right, (43, 248, 243), 1)
-        faces_xywh = str(x),str(y),str(w),str(h)
-        faces_str = str(faces_xywh)
-        cv2.putText(frame,faces_str, (x-15,y-15),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
-        #-- Detectar ojos
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2, y2, w2, h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int((w2 + h2)*0.25)
-            cv2.circle(frame, eye_center, radius, (43, 248, 243), 1)
-        #-- Detectar sonrisas
-        smiles = smile_cascade.detectMultiScale(faceROI, 1.3, 26)
-        for (x3, y3, w3, h3) in smiles:
-            top_left = (x3, y3)
-            botom_right = (x3 + w3, y3 + h3)
-            cv2.rectangle(faceColor, top_left, botom_right, (43, 248, 243), 1)
-            cv2.putText(frame,"Sonriendo", (x-15, y + 250),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
+    #-- Detectar caras de frente
+    ffaces = frontalface_cascade.detectMultiScale(frame_gray, 1.3, 5)
+    if len(ffaces) > 0:
+        for (x, y, w, h) in ffaces:
+            top_left = (x, y)
+            botom_right = (x+w,y+h)
+            faceROI = frame_gray[y:y+h, x:x+w]
+            faceColor = frame[y:y+h, x:x+w]
+            frame = cv2.rectangle(frame, top_left, botom_right, (43, 248, 243), 1)
+            faces_xywh = str(x),str(y),str(w),str(h)
+            faces_str = str(faces_xywh)
+            cv2.putText(frame,faces_str, (x-15,y-15),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
+            #-- Detectar ojos
+            eyes = eyes_cascade.detectMultiScale(faceROI)
+            for (x2, y2, w2, h2) in eyes:
+                eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+                radius = int((w2 + h2)*0.25)
+                cv2.circle(frame, eye_center, radius, (43, 248, 243), 1)
+            #-- Detectar sonrisas
+            smiles = smile_cascade.detectMultiScale(faceROI, 1.3, 26)
+            for (x3, y3, w3, h3) in smiles:
+                top_left = (x3, y3)
+                botom_right = (x3 + w3, y3 + h3)
+                cv2.rectangle(faceColor, top_left, botom_right, (43, 248, 243), 1)
+                cv2.putText(frame,"Sonriendo", (x-15, y + 250),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
+    else:
+        #-- Detectar caras de frente
+        pfaces = profileface_cascade.detectMultiScale(frame_gray, 1.3, 2)
+        for (x, y, w, h) in pfaces:
+            top_left = (x, y)
+            botom_right = (x+w,y+h)
+            faceROI = frame_gray[y:y+h, x:x+w]
+            faceColor = frame[y:y+h, x:x+w]
+            frame = cv2.rectangle(frame, top_left, botom_right, (43, 248, 243), 1)
+            faces_xywh = str(x),str(y),str(w),str(h)
+            faces_str = str(faces_xywh)
+            cv2.putText(frame,faces_str, (x-15,y-15),cv2.FONT_HERSHEY_PLAIN,1, (255, 0, 255),1)
     cv2.imshow('IP Camera - Deteccion de caras', frame)
 #-- Función modificada por Cristian 05-06-2019
 
@@ -107,10 +124,12 @@ class cascada:
         return self.dir_path
 
 #-- Proceso principal
-face_cascades = cascada('D:\\opencv\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface_improved.xml')
+frontalface_cascades = cascada('D:\\opencv\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface_improved.xml')
+profileface_cascades = cascada('D:\\opencv\\opencv\\sources\\data\\lbpcascades\\lbpcascade_profileface.xml')
 eyes_cascades = cascada('D:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
 smile_cascades = cascada('D:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_smile.xml')
-face_cascade = cv2.CascadeClassifier()
+frontalface_cascade = cv2.CascadeClassifier()
+profileface_cascade = cv2.CascadeClassifier()
 eyes_cascade = cv2.CascadeClassifier()
 smile_cascade = cv2.CascadeClassifier()
 url = video(input('Inserte el url de un video de youtube:\n'))
