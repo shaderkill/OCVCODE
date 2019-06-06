5b461479be3ffb720fe60271f5b84ff398680108
shaderkill <cristianmolinaseguel@gmail.com>
2019-06-06 12:48:34 -0400

    Se mejora y comenta OCV_YTDL

--- ocv_youtube/main/OCV_YTDL.py
+++ ocv_youtube/main/OCV_YTDL.py
@@ -1,26 +1,31 @@
+# -- coding: UTF-8 --
+# -- Linter: Pylint --
+# -- Python ver. 3.7.3 --
+
 # -- Imports
 import sys
 from os import path
 from os.path import realpath, normpath
 import cv2
-# -- Llamar imports fuera de la carpeta main
+import PySimpleGUI as sg
+
+# -- Imports fuera de la carpeta
 if __name__ == '__main__':
     sys.path.append(path.join(path.dirname(__file__), '..'))
     from resources import stream
 
 
-# -- Funciones
+# -- Definición de funciones
+
 def tryload():
-    # -- Info CPU, Optimization OPENCV
+
+    # -- Carga de cascadas e info OPENCV/CPU
     print("-"*100)
     print("Hilos:", cv2.getNumThreads())
     print("Nucleos:", cv2.getNumberOfCPUs())
     print("OpenCV optimizado:", cv2.useOptimized())
     print("-"*100)
-    # -- Info CPU modificado por Cristian 04-06-2019
-    # -- Load the cascades
     file = frontalface_cascades.read()
-    print(file)
     if not frontalface_cascade.load(cv2.samples.findFile(file)):
         print(' -- (!)Error cargando "face cascade"')
         exit(0)
@@ -36,9 +41,13 @@ def tryload():
     if not smile_cascade.load(cv2.samples.findFile(file)):
         print(' -- (!)EError cargando "smile cascade"')
         exit(0)
+    playvideo()
+# -- Función modificada por Cristian 06-06-2019
 
 
 def detect_and_display(frame):
+
+    # -- Detección y dibujado sobre el frame capturado
     frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame_gray = cv2.equalizeHist(frame_color)
     # -- Detectar caras de frente
@@ -74,7 +83,7 @@ def detect_and_display(frame):
                             (x-15, y + 250), cv2.FONT_HERSHEY_PLAIN,
                             1, (255, 0, 255), 1)
     else:
-        # -- Detectar caras de frente
+        # -- Detectar caras de perfil
         pfaces = profileface_cascade.detectMultiScale(frame_gray, 1.3, 2)
         for (x, y, w, h) in pfaces:
             top_left = (x, y)
@@ -89,10 +98,22 @@ def detect_and_display(frame):
                         (x-15, y-15), cv2.FONT_HERSHEY_PLAIN,
                         1, (255, 0, 255), 1)
     cv2.imshow('IP Camera - Deteccion de caras', frame)
-# -- Función modificada por Cristian 05-06-2019
+# -- Función modificada por Cristian 06-06-2019
+
+
+def newvideo():
+    url.url = input_video()
+    if url.url is not None:
+        tryload()
+    else:
+        sg.PopupOK('Streaming finalizado', title='OpenCV')
+        exit(0)
+# -- Función creada para cargar un nuevo video.
 
 
 def playvideo():
+
+    # -- Iniciar reproducción del video
     cap = cv2.VideoCapture(stream.readvideo(url.load()))
     cap.set(cv2.CAP_PROP_FPS, 60)
     if not cap.isOpened:
@@ -101,6 +122,7 @@ def playvideo():
     fps = cap.get(cv2.CAP_PROP_FPS)
     print("Frames per second (video): {0}".format(fps))
     while True:
+        sg.PopupAnimated(image_source=None)
         ret, frame = cap.read()
         if frame is None:
             print(' -- (!) No captured frame -- Break!')
@@ -108,20 +130,27 @@ def playvideo():
         cv2.useOptimized()
         detect_and_display(frame)
         if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            newvideo()
             break
 # -- Función modificada por Cristian 04-06-2019
 
 
-# -- Clases
+# -- Clases definidas
 class video:
+
+    # -- Crea la clase video con su respectivo URL
     def __init__(self, url_video):
         self.url = url_video
 
     def load(self):
         return self.url
+# -- Clase modificada por Cristian 05-06-2019
 
 
 class cascada:
+
+    # -- Crea la clase cascada con su respectiva dirección PATH
     def __init__(self, dir_path):
         path_str = r'%s' % dir_path
         path_str = str(path_str).replace('\\', '\\\\')
@@ -129,9 +158,26 @@ class cascada:
 
     def read(self):
         return self.dir_path
+# -- Clase modificada por Cristian 06-06-2019
 
 
-# -- Proceso principal
+def input_video():
+
+    # -- Interfaz para solicitar Input de video
+    url_video = sg.PopupGetText('Inserte un link de youtube',
+                                title='OpenCV en Youtube',
+                                default_text='https://www.youtube.com/...',
+                                background_color='gray90',
+                                text_color='gray10',
+                                font=('Seguoe Ui', '10', 'bold'),
+                                keep_on_top=True,
+                                grab_anywhere=True)
+    return url_video
+# -- Función modificada por Cristian 06-06-2019
+
+# -- Proceso principal --
+
+
 # -- Se crean objetos con sus respectivos atributos
 frontalface_cascades = cascada(normpath(realpath(
     'D:/OpenCV/opencv/sources/data/' +
@@ -149,7 +195,14 @@ frontalface_cascade = cv2.CascadeClassifier()
 profileface_cascade = cv2.CascadeClassifier()
 eyes_cascade = cv2.CascadeClassifier()
 smile_cascade = cv2.CascadeClassifier()
-url = video(input('Inserte el url de un video de youtube:\n'))
+
+# -- Captura inicial de video, solicita URL al usuario
+url = video(input_video())
+if url.url is not None:
+    tryload()
+else:
+    sg.PopupOK('Streaming finalizado', title='OpenCV')
+    exit(0)
+
 # -- Se llaman a las funciones ya definidas
 tryload()
-playvideo()
